188d65d6688f776db51165d762c881ce0a6a4f7d
Michael White <Michael.White@ardot.gov>
2023-07-25 11:11:58 -0500

    unified css style sheet

--- NewMMHIS_Web/Program.cs
+++ NewMMHIS_Web/Program.cs
@@ -15,7 +15,7 @@ namespace NewMMHIS_Web
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+        
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
